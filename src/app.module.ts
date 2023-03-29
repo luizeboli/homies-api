b552da22de75c606615fc32dfc7eab3574fbedef
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2023-03-29 17:23:38 -0300

    refactor: move global guard to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthenticatedGuard } from './auth/utils/authenticated.guard';
 
 @Module({
   imports: [PrismaModule, UsersModule, AuthModule],
+  providers: [{ provide: APP_GUARD, useClass: AuthenticatedGuard }],
 })
 export class AppModule {}
