ab9deea6c5c1ace7cc0bb26d9a08367d495253be
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2023-04-12 22:27:36 -0300

    chore(clerk-setup): remove useless env

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -4,7 +4,6 @@ declare global {
       NODE_ENV: 'development' | 'production' | 'test';
       PORT: string;
       DATABASE_URL: string;
-      CORS_ORIGIN: string;
       CLERK_JWT_PUBLIC_KEY: string;
       CLERK_FRONTEND_API: string;
     }
